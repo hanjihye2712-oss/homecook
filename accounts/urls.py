48952e1dbd3bf7d59f1632c07769b1efe72bf738
han jihye <hanjihye2712@gmail.com>
2026-02-08 16:38:15 +0900

    260208 수정2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name = 'accounts'
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
+    # Django 기본 제공 LoginView 사용
+    path("login/", auth_views.LoginView.as_view(template_name='registration/login.html'), name="login"),
     path("logout/", views.custom_logout, name="custom_logout"), 
 ]
