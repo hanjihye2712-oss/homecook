a150e21af15dfccc9c6b7260ebaffae0297529bd
han jihye <hanjihye2712@gmail.com>
2026-02-11 11:01:45 +0900

    260211-css 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .forms import CustomUserCreationForm
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("login"))
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, "accounts/signup.html", {"form": form})
 
 def custom_logout(request):
